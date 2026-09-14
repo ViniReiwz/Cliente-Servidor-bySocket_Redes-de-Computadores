3d4cf6e94a940ff3486c450bcdb3ff3a97417148
ViniReiwz <vinireisg@usp.br>
2026-09-14 16:01:23 -0300

    Lógica básica do servidor
    Cria o socket e o nomeia (atrela à um socketaddr), fazendo o bind também (em socketutils.c)
    Configura o socket pra que o SO possa reutilizar o endereço (útil pra testes por enquanto porque possibilita encerrar e reinicailizar o programa sem problemas no bind)
    Começa a escutar chamados;
    Aceita uma conexão vinda do cliente;
    Recebe a mensagem do cliente e a exibe;
    Envia uma resposta ao cliente;
    
    Libera memória e fecha os sockets ao final do código.

--- src/gchatsrv.c
+++ src/gchatsrv.c
@@ -1,7 +1,40 @@
 #include "gchat.h"
 
+
 int main()
 {
-    int srvSocketFD = createTCPSocket(SRV_SIDE);
+    // Cria uma socket preparada para o TCP (família AF_INET e do tipo STREAM)
+    int srvSockFD =  socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    setsockopt(srvSockFD,SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    socklen_t addrSize = sizeof(struct sockaddr_in);
+
+    struct sockaddr_in* srvSockAddr = createSocketAddrIPV4(SRV_SIDE, srvSockFD);
+
+    printf("Escutando...\n");
+    listen(srvSockFD, MAX_CONN);
+
+    printf("\nAguardando conexão...\n");
+    struct sockaddr_in* cliSockAddr = (struct sockaddr_in*)calloc(1,sizeof(struct sockaddr_in));
+    int cliSockFD = accept(srvSockFD, (struct sockaddr*)cliSockAddr, &addrSize);
+    printf("\nConexão aceita !\n");
+
+    char buffer[1024];
+    int bytesRec = recv(cliSockFD, buffer, 1023, 0);
+    buffer[bytesRec] = '\0';
+    printf("Mensagem recebida ->> %s\n", buffer);
+    
+    char* response = "Resposta a mensagem de teste !!\0";
+    int bytesSend = send(cliSockFD, response, strlen(response), 0);
+    if(bytesSend == -1){perror("send");}
+
+    close(cliSockFD);
+    close(srvSockFD);
+    free(srvSockAddr);
+    free(cliSockAddr);
+    
+
     return 0;
 }
